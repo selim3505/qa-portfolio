49e8b19fd4de85a51ae4b7684b9d9cfa13590334
selim3505 <selimgulineray@gmail.com>
2025-12-22 20:09:00 +0300

    Add .gitignore and GitHub Actions workflow

---
+++ selenium-java/src/main/java/com/qa/portfolio/utils/ScreenshotUtils.java
@@ -0,0 +1,15 @@
+// ScreenshotUtils.java
+package com.qa.portfolio.utils;
+
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class ScreenshotUtils {
+
+    @Attachment(value = "Screenshot", type = "image/png")
+    public static byte[] takeScreenshot(WebDriver driver) {
+        return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    }
+}
